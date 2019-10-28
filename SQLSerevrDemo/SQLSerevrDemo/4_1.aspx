a01ced8c9835f9cbed909e1837722c67df3072c8
Hola01 <i@mezzp.com>
2019-10-28 10:47:15 +0800

    数据库连接测试

---
+++ SQLSerevrDemo/SQLSerevrDemo/4_1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_1.aspx.cs" Inherits="SQLSerevrDemo._4_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    连接到SQL Server数据库
+    <hr />
+        <asp:Label ID="Label1" runat="server" Text="显示信息"></asp:Label>
+        <br />  <br />
+        <asp:Button ID="btnSM" runat="server" Text="连接到SQL Server" onclick="btnSM_Click" /> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ SQLSerevrDemo/SQLSerevrDemo/4_1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQLSerevrDemo
+{
+    public partial class _4_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSM_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["smdbconn"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                try
+                {
+                    conn.Open();
+                    Label1.Text = "连接字符串: " + conn.ConnectionString;
+                    Label1.Text += "<br/>连接状态: " + conn.State.ToString();
+                    Label1.Text += "<br/>数据源: " + conn.DataSource;
+                    Label1.Text += "<br/>服务器版本: " + conn.ServerVersion;
+                    Label1.Text += "<br/>数据库名称：" + conn.Database;
+                }
+                catch (Exception ee)
+                {
+                    Label1.Text = "连接失败";
+                }
+            }
+
+        }
+    }
+}
