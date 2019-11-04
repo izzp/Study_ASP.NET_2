a751cfa2b0b1e8eb015416040b8d5b39cf54da4d
Hola01 <i@mezzp.com>
2019-11-04 11:07:30 +0800

    4_8

---
+++ DataSetDemo/DataSetDemo/4_8.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_8.aspx.cs" Inherits="DataSetDemo._4_8" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     绑定集合数据
+     <hr />
+     <div class="mid">
+           <div class="left"> 
+           <span class="font">绑定DropDownList</span>  <br />
+           <asp:DropDownList ID="ddlGoodType" runat="server">
+         </asp:DropDownList>   </div>          
+         <div class="left">
+         <span class="font">绑定CheckBoxList</span>  <br />
+         <asp:CheckBoxList ID="chkGoodType" runat="server" RepeatColumns="3" RepeatDirection="Horizontal" Font-Size="Small">
+         </asp:CheckBoxList>   
+     </div>
+    </div> 
+    </form>
+</body></html>
---
+++ DataSetDemo/DataSetDemo/4_8.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//程序名称：4_8.aspx.cs
+//程序功能：显示商品类别信息
+namespace DataSetDemo
+{
+    public partial class _4_8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetBind();
+        }
+        /// <summary>
+        /// 集合数据绑定，将查询的商品类别名称分别
+        /// 绑定到DropDownList控件和CheckBoxList控件
+        /// </summary>
+        protected void SetBind()
+        {
+            string str = ConfigurationManager.ConnectionStrings["smdb"].ConnectionString;
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                string cmdText = "select * from goodsType";
+                SqlDataAdapter sda = new SqlDataAdapter(cmdText, conn);
+                sda.Fill(ds, "gtype");
+            }
+
+            ddlGoodType.DataSource = ds.Tables["gtype"];
+            ddlGoodType.DataTextField = "tName";
+            ddlGoodType.DataValueField = "tID";
+            ddlGoodType.DataBind();
+
+            chkGoodType.DataSource = ds.Tables["gtype"];
+            chkGoodType.DataTextField = "tName";
+            chkGoodType.DataValueField = "tID";
+            chkGoodType.DataBind();
+
+        }
+    }
+}
