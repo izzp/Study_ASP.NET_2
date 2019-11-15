b850c46f15b7afe7e5ed42dc34333d57f4fcf381
hola01 <i@mezzp.com>
2019-11-15 15:37:23 +0800

    未完成_存储过程demo4_12_p148

---
+++ CunChuGuoChengDemo/CunChuGuoChengDemo/4_12.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_12.aspx.cs" Inherits="CunChuGuoChengDemo._4_12" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>添加商品类别</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     <div >
+     使用存储过程，添加商品类别
+     <hr />
+         <asp:Label ID="Label1" runat="server" Text="类别名称"></asp:Label>
+         <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+         <asp:Button ID="btnAddType" runat="server" Text="添加" 
+             onclick="btnAddType_Click" />
+         <br /> <br />
+         <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CunChuGuoChengDemo/CunChuGuoChengDemo/4_12.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CunChuGuoChengDemo
+{
+    public partial class _4_12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddType_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["smdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("upAddGoodsType", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter[] ps ={new SqlParameter("@tname",txtName.Text),
+                                new SqlParameter("@flag",SqlDbType.Int)};
+                ps[1].Direction = ParameterDirection.ReturnValue;
+                cmd.Parameters.AddRange(ps);
+                cmd.ExecuteNonQuery();
+                int tId = (int)cmd.Parameters["@flag"].Value;
+                Label2.Text = txtName.Text + "的类别ID为：" + tId;
+            }
+        }
+    }
+}
