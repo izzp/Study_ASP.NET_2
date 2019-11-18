27cc6e63137d20c63efccbc2e7afd4d4046b8967
hola01 <i@mezzp.com>
2019-11-18 09:31:59 +0800

    重新分类

---
+++ 项目四 使用ADO.NET实现数据访问/ADO.NET/ADO.NET/4_13.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_13.aspx.cs" Inherits="CunChuGCDemo._4_13" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    使用事物，当对某一商品时，该商品的评价数加1<hr/>
+        <asp:Button ID="btnTranTest" runat="server" Text="评价" OnClick="btnTranTest_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ 项目四 使用ADO.NET实现数据访问/ADO.NET/ADO.NET/4_13.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data;
+
+namespace CunChuGCDemo
+{
+    public partial class _4_13 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTranTest_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["smdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                SqlTransaction trans;
+                trans = conn.BeginTransaction();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.Transaction = trans;
+                try
+                {
+                    cmd.CommandText = "insert into GoodEvaluate values(1,4,6,'案例丰富，实用！'," + DateTime.Now.ToString() + ")";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "update Goods set gdEvNum=gdEvNum+1 where gdID=46";
+                    cmd.ExecuteNonQuery();
+                    trans.Commit();
+                    Response.Write("评价成功！");
+                }
+                catch(Exception ee)
+                {
+                    Response.Write(ee.Message);
+                    trans.Rollback();
+                }
+            }
+        }
+    }
+}
